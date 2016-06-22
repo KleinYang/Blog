48dda6ea0c39821385bfee0cc26a28aeda6c9c53
KleinYang <yangyi_shu@qq.com>
2016-06-22 13:03:59 +0800

    init

---
+++ Application/Test/Controller/IndexController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Test\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+      $this->display();
+    }
+}
---
+++ Application/Test/Controller/ManageController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Test\Controller;
+use Think\Controller;
+class ManageController extends Controller {
+    public function index(){
+      $this->display();
+    }
+}
